0da182f2bcd30b1d03f89d551b48751d6bae494d
Arthur D. Andrade <andradedearthur@gmail.com>
2016-12-30 13:09:39 -0200

    New method and refactoring
    Added new method to handle deletion, and to get general information
    about the inventory. Also general refactoring.

--- IvManager/IvManager.Business/Inventory.cs
+++ IvManager/IvManager.Business/Inventory.cs
@@ -8,26 +8,79 @@ namespace IvManager.Business
 {
     public static class Inventory
     {
-        public static List<Product> Products { get; set; }
+        public static List<Product> products;
+        public static List<Product> Products
+        {
+            get
+            {
+                if (products.Count == 0)
+                {
+                    Load();
+                }
+
+                return products;
+            }
+            set { products = value; }
+        }
 
         static Inventory()
         {
             Products = new List<Product>();
         }
-
-        public static void Load()
+       
+        private static void Load()
         {
             Products = DataManager.LoadProducts();
         }
 
-        public static void Save()
+        private static void Save()
         {
             DataManager.SaveProducts(Products);
         }
-        
-        public static void Add(Product produto)
+
+        public static void RemoveProduct(int productId)
         {
-            Products.Add(produto);
+            Inventory.Products.RemoveAll(x => x.Id == productId);
+            Save();
+        }
+
+        public static void Add(Product product)
+        {
+            Products.Add(product);
+            Save();
+        }
+
+        public static int GetNewId()
+        {
+            int id;
+            if (Inventory.Products.Count == 0)
+                id = 1;
+            else
+            {
+                id = Inventory.Products.Last().Id + 1;
+            }
+
+            return id;
+        }
+
+        public static int GetProductCount()
+        {
+            return Inventory.Products.Count();
+        }
+
+        public static int GetUnitCount()
+        {
+            return Inventory.Products.Select(x => x.Quantity).Sum();
+        }
+
+        public static decimal GetInventoryValue()
+        {
+            return Inventory.Products.Select(x => x.Price).Sum();
+        }
+        public static void ClearInventory()
+        {
+            Inventory.Products.Clear();
+            Save();
         }
     }
 }
--- IvManager/IvManager.ConsoleApp/Program.cs
+++ IvManager/IvManager.ConsoleApp/Program.cs
@@ -13,56 +13,149 @@ namespace IvManager.ConsoleApp
     {
         static void Main(string[] args)
         {
+            //The main loop
             bool runAgain = true;
             while (runAgain)
             {
-                Console.WriteLine("======== MENU ========");
-                Console.WriteLine("(1) to list products.");
-                Console.WriteLine("(2) to add products.");
-                Console.WriteLine("(3) to exit.");
-                Console.Write("Input : ");
+                PrintMenu();
                 int input = int.Parse(Console.ReadLine());
 
-                if (input == 1)
+
+
+                switch (input)
                 {
-                    Inventory.Load();
-                    Console.WriteLine("__________________________");
-                    foreach (var item in Inventory.Products)
-                    {
-                        System.Console.WriteLine("{0} | {1} | {2} | {3} ",
-                            item.Id, item.Name, item.Price, item.Quantity);
-                    }
-                }
-                else if (input == 2)
-                {
-                    Console.Write("N° of items to add: ");
-                    int itemsToAdd = int.Parse(Console.ReadLine());
+                    case 0:
+                        runAgain = false;
+                        break;
 
-                    for (int i = 1; i <= itemsToAdd; i++)
-                    {
-                        Console.WriteLine("__________________________");
-                        Console.WriteLine("PRODUCT " + i.ToString());
+                    case 1:
+                        PrintProductsList();
+                        break;
 
-                        Product prod = new Product();
+                    case 2:
+                        Console.Write("N° of items to add: ");
+                        int itemsToAdd = int.Parse(Console.ReadLine());
+                        AddProducts(itemsToAdd);
+                        break;
 
-                        prod.Id = i;
-                        Console.Write("Name: ");
-                        prod.Name = Console.ReadLine();
-                        Console.Write("Price: ");
-                        prod.Price = decimal.Parse(Console.ReadLine());
-                        Console.Write("Quantity: ");
-                        prod.Quantity = int.Parse(Console.ReadLine());
+                    case 3:
+                        PrintProductsList();
+                        Console.Write("ID of item to remove: ");
+                        int itemToRemove = int.Parse(Console.ReadLine());
+                        Inventory.RemoveProduct(itemToRemove);
+                        break;
 
-                        Inventory.Add(prod);
-                    }
-                    Inventory.Save();
-                }
-                else if (input == 3)
-                    runAgain = false;
-                else
-                    Console.WriteLine("Invalid input. Try again. ");
+                    case 4:
+                        Console.WriteLine("Are you sure you want to delete all items?");
+                        Console.WriteLine("(1) - Yes");
+                        Console.WriteLine("(2) - No");
+                        int deleteInput = int.Parse(Console.ReadLine());
+                        HandleInventoryDeletion(deleteInput);
+                        break;
 
+                    case 5:
+                        PrintInventoryInfoMenu();
+                        int infoInput = int.Parse(Console.ReadLine());
+                        PrintIventoryInfo(infoInput);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input. Try again. ");
+                        break;
+                }                    
             }
+        }
+
+        private static void HandleInventoryDeletion(int deleteInput)
+        {
+            switch (deleteInput)
+            {
+                case 1:
+                    Inventory.ClearInventory();
+                    break;
+                case 2:
+                    Console.WriteLine("Inventory was not removed.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
+
+        private static void PrintIventoryInfo(int infoInput)
+        {
+            switch (infoInput)
+            {
+                case 1:
+                    int productCount = Inventory.GetProductCount();
+                    Console.WriteLine($"Product count: {productCount}");
+                    break;
+                case 2:
+                    int unitCount = Inventory.GetUnitCount();
+                    Console.WriteLine($"Unit count: {unitCount}");
+                    break;
+                case 3:
+                    decimal inventoryValue = Inventory.GetInventoryValue();
+                    Console.WriteLine($"Inventory value: {inventoryValue}");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void PrintInventoryInfoMenu()
+        {
+            Console.WriteLine("========= INVENTORY INFO =========");
+            Console.WriteLine("(1) - Products in inventory");
+            Console.WriteLine("(2) - Units in inventory");
+            Console.WriteLine("(3) - Inventory value");
+            Console.WriteLine("(0) - Back");
+            Console.Write("Input: ");
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("======== MENU ========");
+            Console.WriteLine("(1) to list products.");
+            Console.WriteLine("(2) to add products.");
+            Console.WriteLine("(3) to remove a product. ");
+            Console.WriteLine("(4) to delete the inventory. ");
+            Console.WriteLine("(5) to get inventory info. ");
+
+            Console.WriteLine("(0) to exit.");
+            Console.Write("Input : ");
+        }
+
+        private static void AddProducts(int itemsToAdd)
+        {
+            for (int i = 1; i <= itemsToAdd; i++)
+            {
+                Console.WriteLine("__________________________");
+                Console.WriteLine("PRODUCT " + i.ToString());
+
+                Product prod = new Product();
+
+                prod.Id = Inventory.GetNewId();
+                Console.Write("Name: ");
+                prod.Name = Console.ReadLine();
+                Console.Write("Price: ");
+                prod.Price = decimal.Parse(Console.ReadLine());
+                Console.Write("Quantity: ");
+                prod.Quantity = int.Parse(Console.ReadLine());
+
+                Inventory.Add(prod);
+            }
+        }
+
+        private static void PrintProductsList()
+        {
+            Console.WriteLine("__________________________");
+            foreach (var item in Inventory.Products)
+            {
+                Console.WriteLine("{0} | {1} | {2} | {3} ",
+                    item.Id, item.Name, item.Price, item.Quantity);
+            }
+            Console.WriteLine("__________________________");
         }
     }
 }
